c9eec427fa68818d37cb8e16d5195b0117d4ccd7
Megha Agarwal <meghaagr@Meghas-MacBook-Pro.local>
2023-01-09 16:55:34 +0530

    Added Identity server using jwt bearer application

---
+++ src/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using IdentityModel.Client;
+using static System.Console;
+
+WriteLine("Hello, World!");
+
+
+
+var httpClient = new HttpClient();
+var discoveryDocumentAsync = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (discoveryDocumentAsync.IsError)
+{
+    WriteLine("there is an error {0}", discoveryDocumentAsync.Error);
+}
+
+var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+{
+    Address = discoveryDocumentAsync.TokenEndpoint,
+    ClientId = "myClient",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+if (tokenResponse.IsError)
+{
+    WriteLine("there is an error {0}", tokenResponse.Error);
+
+}
+Console.WriteLine("token response {0}", tokenResponse.AccessToken );
+
+var client = new HttpClient();
+client.SetBearerToken(tokenResponse.AccessToken);
+var httpResponseMessage = await client.GetAsync("https://localhost:6001/identity");
+var rootElement = JsonDocument.Parse(await httpResponseMessage.Content.ReadAsStringAsync()).RootElement;
+Console.WriteLine("sdgfsdfgsd {0}",JsonSerializer.Serialize(rootElement));
---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>()
+        {
+            new ApiScope("api1","MyAPI")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>()
+        {
+            new Client()
+            {
+                ClientId = "myClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"api1"}
+            }
+        };
+}
